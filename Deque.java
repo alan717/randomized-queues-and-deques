2f06f4a9f7f83a95828fdd9409096c38a5605d8c
mgruben <GrubenM@gmail.com>
2016-09-17 16:50:44 -0500

    Initialize Deque solution file

---
+++ Deque.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016 Michael <GrubenM@GMail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @author Michael <GrubenM@GMail.com>
+ */
+public class Deque<Item> implements Iterable<Item> {
+   public Deque()                           // construct an empty deque
+   public boolean isEmpty()                 // is the deque empty?
+   public int size()                        // return the number of items on the deque
+   public void addFirst(Item item)          // add the item to the front
+   public void addLast(Item item)           // add the item to the end
+   public Item removeFirst()                // remove and return the item from the front
+   public Item removeLast()                 // remove and return the item from the end
+   public Iterator<Item> iterator()         // return an iterator over items in order from front to end
+   public static void main(String[] args)   // unit testing
+}
