f16ccffd8fc428cd44e6345c3b85a7d570a035df
mgruben <GrubenM@gmail.com>
2016-09-17 17:00:10 -0500

    Use four spaces to tab rather than three

--- Deque.java
+++ Deque.java
@@ -20,45 +20,54 @@
  * @author Michael <GrubenM@GMail.com>
  */
 public class Deque<Item> implements Iterable<Item> {
-   public Deque()                           // construct an empty deque
-   {
-       
-   }
+    public Deque()                           // construct an empty deque
+    {
+        
+    }
    
-   public boolean isEmpty()                 // is the deque empty?
-   {
-       
-   }
-   
-   public int size()                        // return the number of items on the deque
-   {
-       
-   }
-   
-   public void addFirst(Item item)          // add the item to the front
-   {
-       
-   }
-   
-   public void addLast(Item item)           // add the item to the end
-   {
-       
-   }
-   
-   public Item removeFirst()                // remove and return the item from the front
-   {
-       
-   }
-   
-   public Item removeLast()                 // remove and return the item from the end
-   {
-       
-   }
-   
-   public Iterator<Item> iterator()         // return an iterator over items in order from front to end
-   {
-       
-   }
+    public boolean isEmpty()                 // is the deque empty?
+    {
+        
+    }
+    
+    public int size()                        // return the number of items on the deque
+    {
+        
+    }
+    
+    public void addFirst(Item item)          // add the item to the front
+    {
+        
+    }
+    
+    public void addLast(Item item)           // add the item to the end
+    {
+        
+    }
+    
+    public Item removeFirst()                // remove and return the item from the front
+    {
+        
+    }
+    
+    public Item removeLast()                 // remove and return the item from the end
+    {
+        
+    }
+    
+    public Iterator<Item> iterator()         // return an iterator over items in order from front to end
+    {
+        
+    }
+    
+    /**
+     * The Node is the class from which a linked list is built.
+     * The deque here relies on a linked list implementation.
+     */
+    private class Node {
+        Item item;
+        Node next;
+    }
    
    public static void main(String[] args)   // unit testing
 }
