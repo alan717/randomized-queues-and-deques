fa1701e20ee490fa2b7ef687824e7259535ee545
mgruben <GrubenM@gmail.com>
2016-09-17 17:04:11 -0500

    Include specification

--- Deque.java
+++ Deque.java
@@ -16,8 +16,24 @@
  */
 
 /**
- *
- * @author Michael <GrubenM@GMail.com>
+ * A generalization of a stack and a queue that supports adding and removing
+ * items from either the front or the back of the data structure.
+ * 
+ * Corner cases. Throw a java.lang.NullPointerException if the client attempts
+ * to add a null item; throw a java.util.NoSuchElementException if the client
+ * attempts to remove an item from an empty deque; throw a
+ * java.lang.UnsupportedOperationException if the client calls the remove()
+ * method in the iterator; throw a java.util.NoSuchElementException if the
+ * client calls the next() method in the iterator and there are no more items
+ * to return.
+ * 
+ * Performance requirements.   Your deque implementation must support each
+ * deque operation in constant worst-case time. A deque containing n items must
+ * use at most 48n + 192 bytes of memory. and use space proportional to the
+ * number of items currently in the deque. Additionally, your iterator
+ * implementation must support each operation (including construction) in
+ * constant worst-case time.
+ * 
  */
 public class Deque<Item> implements Iterable<Item> {
     public Deque()                           // construct an empty deque
@@ -37,12 +53,12 @@ public class Deque<Item> implements Iterable<Item> {
     
     public void addFirst(Item item)          // add the item to the front
     {
-        
+
     }
     
     public void addLast(Item item)           // add the item to the end
     {
-        
+
     }
     
     public Item removeFirst()                // remove and return the item from the front
